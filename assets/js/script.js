0a14c5c40dba1a682a655d7c6750d277b40ea1d2
T-98 <dkhare@ucsc.edu>
2021-10-29 19:36:46 -0700

    dungeon prototype

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,5 @@ document.getElementById("exp-txt").innerHTML = 20;
 var game = document.getElementById("game");
 
 $("#dungeon").click(function() {
-    console.log("click");
-    let dungeonDiv = $("<div>");
-    let lvlName = $("<p>").attr('id', 'lvlname');
-    $("#lvlname").text('Level');
-    let lvlNameTxt = $("<span>").attr('id', 'lvltxt');
 
-    lvlName.append(lvlNameTxt);
-    dungeonDiv.append(lvlName);
-
-    $("#game").append(dungeonDiv);
 })
