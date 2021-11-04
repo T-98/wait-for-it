bf161d4ae341cc972d98d5f5230ff1fd4eae52d1
DrSlowpokePhd <dohertyjames1999@gmail.com>
2021-11-03 17:57:48 -0700

    added functions to govern game systems.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,23 @@ var game = document.getElementById("game");
 $("#dungeon").click(function() {
 
 })
+
+// important functions for the core systems of the game
+
+// calculate monster hp
+function monster_hp(difficulty_slope, monsters_defeated) {
+    return Math.ciel(difficulty_slope * monsters_defeated + 40);
+}
+
+// amount of gold dropped on monster death
+function gold_drop(difficulty_slope) {
+    return Math.ciel(difficulty_slope * (Math.floor(Math.random() * (6)) + 1));
+}
+
+function weapon_upgrade_price(scale, damage) {
+    return Math.ciel(scale * damage);
+}
+
+function weapon_upgrade_scale(level) {
+    return 2 ^ level/10;
+}
