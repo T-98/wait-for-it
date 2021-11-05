da43471de991e6656d63524e41d27355feb4eedb
James Doherty <dohertyjames1999@gmail.com>
2021-11-04 18:57:22 -0700

    added weapon_gen and weapon_damage functions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,21 @@ function gold_drop(difficulty_slope) {
     return Math.ciel(difficulty_slope * (Math.floor(Math.random() * (6)) + 1));
 }
 
-function weapon_upgrade_price(scale, damage) {
-    return Math.ciel(scale * damage);
+// gold cost to upgrade weapon
+function weapon_upgrade_price(scale) {
+    return Math.ciel(scale * 100);
 }
-
+// scale value to upgrade weapon
 function weapon_upgrade_scale(level) {
     return 2 ^ level/10;
+}
+
+// Damage calculation function
+function damage_calc(difficulty_slope, weapon_damage) {
+    return Math.ciel(difficulty_slope * weapon_damage + 1);
+}
+
+// Weapon damage on generation
+function weapon_gen_damage(x, enemies_defeated) {
+    return x * enemies_defeated;
 }
