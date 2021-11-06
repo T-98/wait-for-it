5f84aff0b786db8db9d85165ea94109546ac602f
DrSlowpokePhd <dohertyjames1999@gmail.com>
2021-11-05 19:43:04 -0700

    added game class to facilitate the save/load function through localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,35 @@ function damage_calc(difficulty_slope, weapon_damage) {
 }
 
 // Weapon damage on weapon generation
-function weapon_gen_damage(, enemies_defeated) {
+function weapon_gen_damage(x, enemies_defeated) {
     return x * enemies_defeated;
 }
+
+// game object
+// handles all values relevant to game
+class Game {
+    constructor(difficulty_slope = 0.5, inventory = {}, current_weapon = {}, monsters_defeated = 0, gold = 0) {
+        this.difficulty_slope = difficulty_slope;
+        this.inventory = inventory;
+        this.current_weapon = current_weapon;
+        this.monsters_defeated = monsters_defeated;
+        this.gold = gold;
+    }
+
+    save() {
+        localStorage.setItem('difficulty_slope', this.difficulty_slope);
+        localStorage.setItem('inventory', this.inventory);
+        localStorage.setItem('current_weapon', this.current_weapon);
+        localStorage.setItem('monsters_defeated', this.monsters_defeated);
+        localStorage.setItem('gold', this.gold);
+    }
+
+    load() {
+        this.difficulty_slope = localStorage.getItem('difficulty_slope');
+        this.inventory = localStorage.getItem('inventory');
+        this.current_weapon = localStorage.getItem('current_weapon');
+        this.monsters_defeated = localStorage.getItem('monsters_defeated');
+        this.gold = localStorage.getItem('gold');
+    }
+}
+
